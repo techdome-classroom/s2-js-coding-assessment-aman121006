e7202707d810f782e59d11043c577d27c997e63d
Aman Kumar <amankumarsharma7000@gmail.com>
2024-10-08 14:07:25 +0530

    Done

--- program1.js
+++ program1.js
@@ -3,9 +3,29 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    // Dictionary to map closing brackets to corresponding opening brackets
+    const bracketMap = { ')': '(', '}': '{', ']': '[' };
+    // Stack to keep track of opening brackets
+    const stack = [];
     
+    // Iterate through each character in the string
+    for (let char of s) {
+        // If it's a closing bracket
+        if (char in bracketMap) {
+            // Pop from stack if not empty, else assign a dummy value
+            const topElement = stack.length > 0 ? stack.pop() : '#';
+            // Check if the popped element is the correct opening bracket
+            if (bracketMap[char] !== topElement) {
+                return false;
+            }
+        } else {
+            // If it's an opening bracket, push onto the stack
+            stack.push(char);
+        }
+    }
+    
+    // Check if the stack is empty
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
--- program2.js
+++ program2.js
@@ -3,8 +3,23 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    // Dictionary to map Roman numerals to integers
+    const romanMap = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 };
+    // Variable to store the integer result
+    let result = 0;
     
+    // Traverse through the string
+    for (let i = 0; i < s.length; i++) {
+        // If the current numeral is less than the next numeral, subtract its value
+        if (i < s.length - 1 && romanMap[s[i]] < romanMap[s[i + 1]]) {
+            result -= romanMap[s[i]];
+        } else {
+            // Otherwise, add its value
+            result += romanMap[s[i]];
+        }
+    }
+    
+    return result;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
